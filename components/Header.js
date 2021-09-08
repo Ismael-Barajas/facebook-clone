41fbe19b851cf40072cc2558d90e28c353f05689
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-09-07 23:40:17 -0700

    Header Complete

--- components/Header.js
+++ components/Header.js
@@ -14,8 +14,10 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import HeaderIcon from "./HeaderIcon";
+import { signOut, useSession } from "next-auth/client";
 
 const Header = () => {
+  const [session] = useSession();
   return (
     <div
       className="sticky top-0 z-50 bg-white flex
@@ -33,7 +35,7 @@ const Header = () => {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none 
+            className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none 
             placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook Clone"
@@ -50,7 +52,27 @@ const Header = () => {
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
-      {/* Right https://youtu.be/dBotWYKYYWc?t=3461 */}
+      {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile pic */}
+        <Image
+          onClick={() => signOut()}
+          className="rounded-full cursor-pointer"
+          src={session.user.image}
+          width={40}
+          height={40}
+          layout="fixed"
+          alt={session.user.name}
+        />
+
+        <p className="whitespace-nowrap font-semibold pr-3">
+          {session.user.name}
+        </p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 };
